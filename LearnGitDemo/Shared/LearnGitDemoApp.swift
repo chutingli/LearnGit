ad5bba98ec60943288faf1371ac619446ac173b3
chuting <chuting@chutingdeMBP.lan>
2021-08-31 00:27:10 -0400

    First Commit

---
+++ LearnGitDemo/Shared/LearnGitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitDemoApp.swift
+//  Shared
+//
+//  Created by chuting on 2021/8/31.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
